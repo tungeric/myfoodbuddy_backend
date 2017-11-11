a8eaf35dc27227b749a938edf9f08cf18ac25731
Eric Tung <eric.p.tung@gmail.com>
2017-11-11 02:14:12 -0800

    Lets try rack cors again

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#root'
-
-  post "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
 end
