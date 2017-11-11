25c4eb3a5debdda6031668f315ad457d4c3a9c60
Eric Tung <eric.p.tung@gmail.com>
2017-11-11 02:07:16 -0800

    More cors testing

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#root'
+
+  match "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
 end
