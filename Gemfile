e86ae3972ef2789a2996af45a3b10b11b073c255
Eric Tung <eric.p.tung@gmail.com>
2017-11-11 01:17:40 -0800

    Attempt to solve cors again

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'pry-rails'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'rails_12factor'
+gem 'rack-cors'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,12 @@ module MyfoodbuddyBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.action_dispatch.default_headers.merge!({
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Request-Method' => '*'
-    })
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => :any
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
