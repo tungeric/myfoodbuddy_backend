fa84c53f503e2c27a933b12785e37c12bb51beb8
Eric Tung <eric.p.tung@gmail.com>
2017-11-11 01:20:54 -0800

    Try solving cors again

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,10 @@ module MyfoodbuddyBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.middleware.insert_before 0, "Rack::Cors" do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => :any
-      end
-    end
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
