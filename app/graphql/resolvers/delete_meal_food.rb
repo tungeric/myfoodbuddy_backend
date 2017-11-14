ee4e74a65854fc9379c3db3605fd4ae40829a070
Eric Tung <eric.p.tung@gmail.com>
2017-11-13 18:26:15 -0800

    Fix delete mealfood

--- app/graphql/resolvers/delete_meal_food.rb
+++ app/graphql/resolvers/delete_meal_food.rb
@@ -1,10 +1,11 @@
 class Resolvers::DeleteMealFood < GraphQL::Function
-  argument :mealfood_id, !types.Int
+  argument :food_id, !types.Int
+  argument :meal_id, !types.Int
 
   type Types::MealFoodType
 
   def call(obj, args, ctx)
-    meal_food = MealFood.find(args[:mealfood_id])
+    meal_food = MealFood.where(:food_id=>args[:food_id]).where(:meal_id=>args[:meal_id]).first
     meal_food.destroy!
   rescue ActiveRecord::RecordInvalid => e
     GraphQL::ExecutionError.new("Invalid input: #{e.record.full_messages.join(', ')}")
