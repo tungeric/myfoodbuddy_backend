537b5ccac433192e42e8e21b02617ea43870d7db
Eric Tung <eric.p.tung@gmail.com>
2017-11-25 11:36:18 -0800

    College meal average and total data

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -16,6 +16,7 @@ class Meal < ApplicationRecord
   has_many :meal_foods, dependent: :destroy
   has_many :foods, through: :meal_foods
 
+
   def ensure_meal_time
     self.meal_time ||= Time.now
   end
@@ -23,4 +24,95 @@ class Meal < ApplicationRecord
   def meal_time_since_epoch
     self.meal_time.to_f * 1000
   end
+
+  # MEAL COLLECTED DATA
+
+  def meal_calories
+    total_calories = 0
+    self.foods.each do |food|
+      total_calories += food.calories
+    end
+    total_calories
+  end
+
+  def meal_protein
+    total_protein = 0
+    self.foods.each do |food|
+      total_protein += food.protein
+    end
+    total_protein
+  end
+
+  def meal_carbs
+    total_carbs = 0
+    self.foods.each do |food|
+      total_carbs += food.carbs
+    end
+    total_carbs
+  end
+
+  def meal_fat
+    total_fat = 0
+    self.foods.each do |food|
+      total_fat += food.fat
+    end
+    total_fat
+  end
+
+  # TOTAL COLLECTED DATA
+
+  def self.total_calories
+    total_calories = 0
+    all_meals = Meal.all
+    all_meals.each do |meal|
+      total_calories += meal.meal_calories
+    end
+    total_calories
+  end
+
+  def self.total_protein
+    total_protein = 0
+    all_meals = Meal.all
+    all_meals.each do |meal|
+      total_protein += meal.meal_protein
+    end
+    total_protein
+  end
+
+  def self.total_carbs
+    total_carbs = 0
+    all_meals = Meal.all
+    all_meals.each do |meal|
+      total_carbs += meal.meal_carbs
+    end
+    total_carbs
+  end
+
+  def self.total_fat
+    total_fat = 0
+    all_meals = Meal.all
+    all_meals.each do |meal|
+      total_fat += meal.meal_fat
+    end
+    total_fat
+  end
+
+  # AVERAGE COLLECTED DATA
+
+  def self.average_calories_per_meal
+    Meal.total_calories / Meal.count
+  end
+
+  def self.average_protein_per_meal
+    Meal.total_protein / Meal.count
+  end
+
+  def self.average_carbs_per_meal
+    Meal.total_carbs / Meal.count
+  end
+
+  def self.average_fat_per_meal
+    Meal.total_fat / Meal.count
+  end
+
 end
