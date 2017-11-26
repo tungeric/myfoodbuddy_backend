f40926e21d5946351bcf3f044767c2718946ad3f
Eric Tung <eric.p.tung@gmail.com>
2017-11-26 00:02:48 -0800

    Create food averageness index to find the most average food based on the items consumed

--- app/models/food.rb
+++ app/models/food.rb
@@ -19,4 +19,18 @@ class Food < ApplicationRecord
 
   has_many :meal_foods, dependent: :destroy
   has_many :meals, through: :meal_foods
+
+  def averageness_index
+    calories_diff = (self.calories - Meal.average_calories_per_food).to_f / Meal.average_calories_per_food
+    protein_diff = (self.protein - Meal.average_protein_per_food).to_f / Meal.average_protein_per_food
+    carbs_diff = (self.carbs - Meal.average_carbs_per_food).to_f / Meal.average_carbs_per_food
+    fat_diff = (self.fat - Meal.average_fat_per_food).to_f / Meal.average_fat_per_food
+    total_diff = (calories_diff + protein_diff + carbs_diff + fat_diff)
+    averageness = 1 - (total_diff.to_f / 4)
+    averageness
+    # (self.calories / Meal.average_calories_per_food + 
+    # self.protein / Meal.average_protein_per_food + 
+    # self.carbs / Meal.average_carbs_per_food + 
+    # self.fat / Meal.average_fat_per_food).to_f / 4
+  end
 end
